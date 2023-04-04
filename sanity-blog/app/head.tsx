c4e5d0014473144113646b1fdf5f7ddf4c19d12f
lb <lbdeyo@gmail.com>
2023-04-04 17:33:15 -0500

    initial commit

---
+++ sanity-blog/app/head.tsx
@@ -0,0 +1,11 @@
+import {title} from "process";
+
+export default function Head() {
+  return (
+    <>
+      <title>L.B. Deyo, Designer</title>
+      <meta content="width=device-width, initial-scale=1" name="viewpoert" />
+      <link rel="stylesheet" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ sanity-blog/sanity.config.ts
@@ -0,0 +1,27 @@
+import {defineConfig, isDev} from "sanity";
+import {visionTool} from "@sanity/vision";
+import {deskTool} from "sanity/desk";
+import {schemaTypes} from "./schemas";
+
+// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+// const dataset = process.env.PUBLIC_SANITY_DATASET;
+
+// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const projectId = "bx055f5z";
+const dataset = "production";
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  basePath: "/studio",
+  name: "PAPAFAM_Content_Studio",
+  title: "PAPAFAM_Content_Studio",
+
+  projectId,
+  dataset,
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
