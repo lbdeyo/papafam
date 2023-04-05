3de053026f0ab49c232c0007d100b537de8e12a0
lb <lbdeyo@gmail.com>
2023-04-04 19:24:06 -0500

    Fixing env

--- sanity-blog/app/page.tsx
+++ sanity-blog/app/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 function HomePage() {
+  const projectId = process.env.TEST_VAR;
+  console.log("TESTVAR IS", projectId);
   return (
     <div>
       <h1 className="text-4xl">Welcome to the Blog</h1>
-      <p>Some content</p>
+      <p>FUCK{projectId}</p>
     </div>
   );
 }
--- sanity-blog/sanity.config.ts
+++ sanity-blog/sanity.config.ts
@@ -3,12 +3,12 @@ import {visionTool} from "@sanity/vision";
 import {deskTool} from "sanity/desk";
 import {schemaTypes} from "./schemas";
 
-// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-// const dataset = process.env.PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.PUBLIC_SANITY_DATASET;
 
 // const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const projectId = "bx055f5z";
-const dataset = "production";
+// const projectId = "bx055f5z";
+// const dataset = "production";
 // const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
