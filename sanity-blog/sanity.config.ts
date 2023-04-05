9569e059fb400bc3adc184562b51e6d36c8b1011
lb <lbdeyo@gmail.com>
2023-04-04 19:25:04 -0500

    Another related fix

--- sanity-blog/sanity.config.ts
+++ sanity-blog/sanity.config.ts
@@ -6,11 +6,6 @@ import {schemaTypes} from "./schemas";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.PUBLIC_SANITY_DATASET;
 
-// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-// const projectId = "bx055f5z";
-// const dataset = "production";
-// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-
 export default defineConfig({
   basePath: "/studio",
   name: "PAPAFAM_Content_Studio",
