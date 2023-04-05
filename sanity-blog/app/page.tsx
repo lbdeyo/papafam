e39884b5241387c53fb18517c2caef365cded9c8
lb <lbdeyo@gmail.com>
2023-04-04 21:49:48 -0500

    Cleaning up

--- sanity-blog/app/page.tsx
+++ sanity-blog/app/page.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 function HomePage() {
-  const projectId = process.env.TEST_VAR;
-  console.log("TESTVAR IS", projectId);
   return (
     <div>
       <h1 className="text-4xl">Welcome to the Blog</h1>
-      <p>FUCK{projectId}</p>
+      <p>FUCK{}</p>
     </div>
   );
 }
--- sanity-blog/sanity.config.ts
+++ sanity-blog/sanity.config.ts
@@ -4,8 +4,7 @@ import {deskTool} from "sanity/desk";
 import {schemaTypes} from "./schemas";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.PUBLIC_SANITY_DATASET;
-
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export default defineConfig({
   basePath: "/studio",
   name: "PAPAFAM_Content_Studio",
