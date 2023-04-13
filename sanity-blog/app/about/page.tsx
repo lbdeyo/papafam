b06688bf7a5b8573b02168cf8dc2fbeaef818232
lb <lbdeyo@gmail.com>
2023-04-13 14:17:54 -0500

    Trying in vain to add drop shadows to text

--- sanity-blog/app/about/page.tsx
+++ sanity-blog/app/about/page.tsx
@@ -36,11 +36,11 @@ async function About({params: {slug = "editorial-maps"}}: Props) {
   const post: Post = await client.fetch(query, {slug});
 
   return (
-    <section className="mb-10  mt-24  mx-auto px-10  dark:bg-blend-multiply max-w-4xl fade-in-2  ">
+    <section className="mb-10  mt-24  mx-auto px-10   max-w-4xl fade-in-2  ">
       <div className="  max-w-screen-2xl ">
         <div>
           <h1 className="text-4xl md:text-5xl  mb-0 pb-0 ">About L.B. Deyo</h1>
-          <div className="border-b-slate-400 dark:border-b-[#ffae00] border-b-2 mt-2"></div>
+          <div className="border-b-slate-400 border-dashed  dark:border-b-[#ffae00] dark:shadow-black dark:shadow-sm border-b-2 mt-2"></div>
           <div className=" lg:flex lg:flex-row lg:justify-between">
             <Image
               className="mt-10 lg:mr-10 object-cover object-top-center mx-aut rounded-xl"
@@ -98,7 +98,7 @@ first-letter:mr-3 first-letter:float-left">
               </div>
               <Link
                 href="/contact"
-                className="text-black bg-[#ffae00] hover:bg-[#ffcc00] focus:ring-4 focus:ring-blue-300 hover:text-black font-medium rounded-lg text-sm px-5 py-2.5 mr-2 focus:outline-none dark:focus:ring-blue-800">
+                className="text-black bg-[#ffae00] hover:bg-[#ffcc00] focus:ring-4 focus:ring-blue-300 hover:text-black font-medium rounded-lg text-sm px-5 py-2.5 text-bold mr-2 focus:outline-none dark:focus:ring-blue-800">
                 LET&apos;S TELL YOUR STORY
               </Link>
               <div className="m-5"></div>
--- sanity-blog/components/Jumbotron.tsx
+++ sanity-blog/components/Jumbotron.tsx
@@ -3,27 +3,27 @@ import React from "react";
 
 function Jumbotron() {
   return (
-    <section className="mx-10 mb-10  mt-24  max-w-5xl fade-in-2  ">
+    <section className="mx-10 mb-10  mt-24  max-w-5xl fade-in-2   ">
       <div className="  max-w-screen-2xl ">
         <div>
-          <h1 className="text-4xl md:text-5xl  mb-0 pb-0 ">
+          <h1 className="text-4xl md:text-5xl  mb-0 pb-0   ">
             Pro Design & Development
           </h1>
-          <div className="border-b-slate-400 dark:border-b-[#ffae00] border-b-2 mt-2"></div>
+          <div className="border-b-slate-400 border-dashed  dark:border-b-[#ffae00] dark:shadow-black dark:shadow-sm border-b-2 mt-2"></div>
           <p className="mt-2 md:mt-3 text-3xl mb-3">
             Creating multimedia, websites and more.
           </p>
           <p
             className=" md:text-justify first-line:uppercase md:first-line:tracking-widest
   first-letter:text-6xl  
-  first-letter:mr-3 first-letter:float-left lg:columns-2  gap-6">
+  first-letter:mr-3 first-letter:float-left lg:columns-2  gap-6  drop-shadow-md drop-shadow-black ">
             Web design and development. Motion graphics and video. Print. Social
             Media. L.B. Deyo has created work for major clients like Ford Motor
             Company, Jane magazine, Cisco Systems, Universal Studios, The Plaza
             Hotel, and Calvin Klein. All these clients had stories to tell about
             their products and services. Now it&apos;s time to tell your story.
           </p>
-          <div className="border-b-slate-400 dark:border-zinc-700 border-b-2 my-5"></div>
+          <div className="border-b-slate-300 dark:border-zinc-700 border-b-2 my-5"></div>
           <h1 className="text-3xl">Services</h1>
           <ul className="mb-6 mt-3  list-disc pl-4 lg:columns-2  darK:text-slate-200  gap-6 ">
             <li>
@@ -47,7 +47,7 @@ function Jumbotron() {
           </ul>
           <Link
             href="/contact"
-            className="text-black bg-[#ffae00] hover:bg-[#ffcc00]  focus:ring-4 focus:ring-blue-300 hover:text-black font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  focus:outline-none dark:focus:ring-blue-800">
+            className="text-black bg-[#ffae00] hover:bg-[#ffcc00]  focus:ring-4 focus:ring-blue-300 hover:text-black font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  focus:outline-none text-bold dark:focus:ring-blue-800">
             GET IN TOUCH
           </Link>
         </div>
