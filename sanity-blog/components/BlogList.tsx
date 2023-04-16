46f35446ff593bc42abf91228db7920e9c7d360e
lb <lbdeyo@gmail.com>
2023-04-15 19:55:44 -0500

    Creating BlogCard component

--- sanity-blog/components/BlogList.tsx
+++ sanity-blog/components/BlogList.tsx
@@ -1,8 +1,8 @@
 import {ArrowUpRightIcon} from "@heroicons/react/24/solid";
-import Image from "next/image";
-import urlFor from "../lib/urlFor";
+
 import ClientSideRoute from "./ClientSideRoute";
 import Jumbotron from "./Jumbotron";
+import BlogCard from "./BlogCard";
 
 type Props = {
   posts: Post[];
@@ -21,24 +21,11 @@ export default function BlogList({posts}: Props) {
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
           <ClientSideRoute route={`/post/${post.slug.current}`} key={post._id}>
-            <span className="flex flex-col  group border-slate-500 fade-in-4  border-opacity-25 cursor-pointer  hover:shadow-[0_35px_60px_-8px_rgba(0,0,0,0.4)] ">
-              <div className="relative w-full h-80 ">
-                <Image
-                  className="object-cover object-center lg:object-center border-black rounded-t-[20px]"
-                  src={urlFor(post.mainImage).url()}
-                  alt={post.author.name}
-                  fill
-                />
-              </div>
-              <div className="p-3 pb-5 pt-0">
-                <div className="mt-5 flex-1">
-                  <p className="  text-lg font-bold">{post.title}</p>
-                  <p className="line-clamp-2 text-gray-500">
-                    {post.description}
-                  </p>
-                </div>
-              </div>
-            </span>
+            <BlogCard
+              title={post.title}
+              imgUrl={post.mainImage.asset}
+              description={post.description}
+            />
           </ClientSideRoute>
         ))}
       </div>
