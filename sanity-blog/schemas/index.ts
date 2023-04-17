6b148ea164f231709236de4ad0d9a1cb924f51fe
lb <lbdeyo@gmail.com>
2023-04-17 18:38:36 -0500

    Still adding the image schema

--- sanity-blog/schemas/index.ts
+++ sanity-blog/schemas/index.ts
@@ -3,5 +3,13 @@ import category from "./category";
 import post from "./post";
 import author from "./author";
 import youtube from "./youtube";
+import simpleImage from "./simpleImage";
 
-export const schemaTypes = [post, author, category, blockContent, youtube];
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  youtube,
+  simpleImage,
+];
