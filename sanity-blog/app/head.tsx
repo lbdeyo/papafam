19d72169ea67e1d561224adc57a973276ea3fa6d
lb <lbdeyo@gmail.com>
2023-04-17 21:39:44 -0500

    Trying to get the preview image going

--- sanity-blog/app/head.tsx
+++ sanity-blog/app/head.tsx
@@ -5,6 +5,10 @@ export default function Head() {
     <>
       <title>L.B. Deyo, Designer</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        property="og:image"
+        content="https://cdn.sanity.io/images/bx055f5z/production/93408f373027dd95b7a5d4a9f162ec1a08984459-2344x1300.png?w=2000&fit=max&auto=format&dpr=2"
+      />
     </>
   );
 }
