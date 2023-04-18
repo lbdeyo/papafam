28fcc495c0e038ad926af6c43844ab638aa963e2
lb <lbdeyo@gmail.com>
2023-04-17 22:41:30 -0500

    Fixing twitter preview

--- sanity-blog/app/head.tsx
+++ sanity-blog/app/head.tsx
@@ -9,6 +9,20 @@ export default function Head() {
         property="og:image"
         content="https://cdn.sanity.io/images/bx055f5z/production/93408f373027dd95b7a5d4a9f162ec1a08984459-2344x1300.png?w=2000&fit=max&auto=format&dpr=2"
       />
+      <meta
+        property="og:description"
+        content="Full service web design and full-stack web development"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:description"
+        content="Full service web design and full-stack web development"
+      />
+      <meta name="twitter:title" content="L.B. Deyo, Designer" />
+      <meta
+        name="twitter:image"
+        content="https://cdn.sanity.io/images/bx055f5z/production/93408f373027dd95b7a5d4a9f162ec1a08984459-2344x1300.png?w=2000&fit=max&auto=format&dpr=2"
+      />
     </>
   );
 }
