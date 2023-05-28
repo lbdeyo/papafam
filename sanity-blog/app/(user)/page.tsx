cebbade66c53552e9979d1a0ace5ce25c76c4a4d
lb <lbdeyo@gmail.com>
2023-05-27 21:15:39 -0500

    Trying to fix this crap after upgrading Next

--- sanity-blog/app/(user)/page.tsx
+++ sanity-blog/app/(user)/page.tsx
@@ -1,11 +1,6 @@
-import {draftMode} from "next/headers";
-import PreviewSuspense from "../../components/PreviewSuspense";
 import BlogList from "../../components/BlogList";
-import PreviewBlogList from "../../components/PreviewBloglist";
 import {client} from "../../lib/sanity.client";
 import {groq} from "next-sanity";
-
-import {SpeakerWaveIcon} from "@heroicons/react/24/solid";
 
 const query = groq`*[_type=='post'] {
    ...,
@@ -15,21 +10,6 @@ const query = groq`*[_type=='post'] {
 `;
 export const revalidate = 60; //revalidate every 60 seconds
 export default async function IndexPage() {
-  if (draftMode()) {
-    return (
-      <PreviewSuspense
-        fallback={
-          <div role="status">
-            <p className="text-center text-lg animate-pulse text-[#ffae00]">
-              Loading Preview Data...
-            </p>
-          </div>
-        }>
-        <PreviewBlogList query={query} />
-      </PreviewSuspense>
-    );
-  }
-
   const posts = await client.fetch(query);
 
   return <BlogList posts={posts} />;
