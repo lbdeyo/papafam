818171c98c7804f0b2fbeaa89ae871e859ed5615
L.B. Deyo <lbdeyo@gmail.com>
2024-12-28 11:42:48 -0600

    Adding detailed skills.

--- components/Jumbotron.tsx
+++ components/Jumbotron.tsx
@@ -25,9 +25,8 @@ function Jumbotron() {
               Web design and development. Motion graphics and video. Print.
               Social Media. L.B. Deyo has created work for major clients like
               Ford Motor Company, Jane magazine, Cisco Systems, Universal
-              Studios, The Plaza Hotel, and Calvin Klein. All these clients had
-              stories to tell about their products and services. Now it&apos;s
-              time to tell your story.
+              Studios, The Plaza Hotel, and Calvin Klein. Now it&apos;s time to
+              tell your story.
             </p>
           </div>
         </div>
@@ -35,29 +34,60 @@ function Jumbotron() {
       <div className="border-b-zinc-500 shadow-black shadow-sm border-b my-5"></div>
       <div className="w-full">
         <h1 className="text-3xl">Services</h1>
-        <div className="max-w-3xl">
-          <ul className="mb-6 mt-3 grid grid-cols-1 md:grid-cols-2 gap-4 text-slate-200 list-disc pl-5">
-            <li>
-              <Link href="/about">Print and digital design</Link>
-            </li>
-            <li>
-              <Link href="/about">
+        <div className="max-w-5xl">
+          <div className="mb-6 mt-3 columns-1 md:columns-2 gap-8 space-y-4">
+            <p>
+              <span className="text-white opacity-100">
+                Digital and print design
+              </span>{" "}
+              <span className="opacity-75">
+                with Adobe Photoshop, Adobe Illustrator, Adobe InDesign, Figma,
+                and other applications.
+              </span>
+            </p>
+
+            <p>
+              <span className="text-white opacity-100">
+                Custom illustration
+              </span>
+              <span className="opacity-75">, freehand and digital.</span>
+            </p>
+
+            <p>
+              <span className="text-white opacity-100">
                 Coding with modern standards-based technologies
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">Video and motion graphics</Link>
-            </li>
-            <li>
-              <Link href="/about">Sound editing</Link>
-            </li>
-            <li>
-              <Link href="/about">Custom illustration</Link>
-            </li>
-            <li>
-              <Link href="/about">Copywriting</Link>
-            </li>
-          </ul>
+              </span>
+              <span className="opacity-75">
+                , including HTML, CSS, JavaScript, React, Next.js, Tailwind CSS,
+                and more.
+              </span>
+            </p>
+
+            <p>
+              <span className="text-white opacity-100">
+                Video and motion graphics
+              </span>{" "}
+              <span className="opacity-75 inline">
+                production (shooting, sound recording) and postproduction with
+                tools like Adobe Premiere, Adobe After Effects, Final Cut Pro
+                and Cinema 4D.
+              </span>
+            </p>
+
+            <p>
+              <span className="text-white opacity-100">Copywriting</span>&nbsp;
+              <span className="opacity-75">for editorial and advertising.</span>
+            </p>
+
+            <p>
+              <span className="text-white opacity-100">Sound editing</span>
+              &nbsp;
+              <span className="opacity-75">
+                in Logic Pro X and Adobe Audition.
+              </span>
+            </p>
+          </div>
+
           <Link
             href="/contact"
             className="text-black bg-[#ffae00] hover:bg-[#ffcc00] focus:ring-4 hover:text-black font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none text-bold focus:ring-blue-800">
