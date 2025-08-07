f1771b1ac40e1a2fdcad43c5caec1551d23d7753
lbdeyo <lbdeyo@gmail.com>
2025-08-07 10:31:30 -0500

    Adding Google Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,25 @@
 import "../styles/globals.css";
 import React from "react";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="font-sans">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BBJ534GQEW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BBJ534GQEW');
+          `}
+        </Script>
+      </head>
       <body className="font-cormorant">
         <nav className="bg-[#15232c]">{/* Navigation content */}</nav>
         {children}
